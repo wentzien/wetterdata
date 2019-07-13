f3c35760bf8781d14ed5cff125fa7d67c4116f9a
Simon <tobertsi@hs-pforzheim.de>
2019-07-14 00:12:51 +0200

    Favoriten im Menü sichtbar gemacht
    Beim draufklicken wird der heute Task mit der Stationsid per POST Methode übergeben

--- includes/main_navbar.php
+++ includes/main_navbar.php
@@ -2,4 +2,43 @@
     <a href="?task=home" data-role="button" data-icon="home" data-theme="b" data-ajax="false" >Home</a>
     <a href="?task=heute" data-role="button" data-icon="home" data-theme="b" data-ajax="false" >Heute</a>
     <a href="?task=editfav" data-role="button" data-icon="heart" data-theme="b" data-ajax="false" >Favoriten</a>
+
+    <?php
+    $uID=Core::$user->m_oid;
+    if ($uID <>""){
+        $pdo = Core::$pdo;
+
+
+    $SQLaddfav = "SELECT * FROM favoriten LEFT JOIN Stationen ON favoriten.Stationid = Stationen.ID Where UserID=$uID";
+    $ufavs=$pdo->query($SQLaddfav);
+    ?>
+    
+    
+        
+    
+    
+
+         <?php
+        foreach ($ufavs as $station){
+            ?>
+            <form action="?task=heute" method="post">
+            <input type="hidden" id="stat" name="ausgewStation" value="<?=$station['id']?>">
+            <input type="submit" name="station" value="<?=$station['stationsname']?>Hallo">
+            </form>
+            
+            <?php            
+        }
+            
+    }?>
+    
 </div>
+
+
+
+
+<!--<a href="?task=heute" data-role="button" data-icon="heart" data-theme="b" data-ajax="false">
+         <?php
+        foreach ($Ufavs as $station){
+            echo("<option value=".$station['id']." >".$station['stationsname']."</option>\n");
+        }?>
+</a>-->
