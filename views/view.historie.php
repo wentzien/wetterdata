674253d836fcba0c986670bade73afb98373f680
Dennis Wentzien <wentzien@users.noreply.github.com>
2019-07-17 15:03:43 +0200

    controller und view historie geaddet

---
+++ views/view.historie.php
@@ -0,0 +1,94 @@
+<!DOCTYPE HTML>
+<html>
+<head>  
+<script>
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Das heutige Wetter"
+	},
+	axisX: {
+		valueFormatString: "HH:mm"
+	},
+	axisY: {
+		title: "Temperature (in °C)",
+		includeZero: false,
+		suffix: " °C"
+	},
+	legend:{
+		cursor: "pointer",
+		fontSize: 16,
+		itemclick: toggleDataSeries
+	},
+	toolTip:{
+		shared: true
+	},
+	data: [
+            <?php 
+            $length=core::$view->length;
+            $length=$length-1;
+            for($x=0; $x<=$length; $x++){
+            echo('{
+		name: "');
+                        //Ausgabe des Stationsnamen
+                        $row1="";
+                        $stationNameNummer="stationName$x";
+                        $stationName=core::$view->$stationNameNummer;
+                        foreach($stationName as $row1){
+                        echo($row1['stationsname']);
+                        }   
+                        //Ende Ausgabe des Stationsnamen
+            echo('",
+		type: "spline",
+		yValueFormatString: "#0.## °C",
+		showInLegend: true,
+		dataPoints: ['); 
+                        //Ausgabe der Temp Werte
+                        $row2="";
+                        $heuteTempNummer="heuteTemp$x";
+                        $heuteTemp=core::$view->$heuteTempNummer;
+                        $i="1";
+                        foreach ($heuteTemp as $row2){
+                        echo("{ x: new Date (".$row2['canvasts']."), y: ".$row2['temp20']." },\n");
+                        }
+                        //Ende der Ausgabe der Temp Werte
+            echo(']
+            },'); 
+            }
+            ?>
+    ]
+});
+chart.render();
+
+function toggleDataSeries(e){
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	}
+	else{
+		e.dataSeries.visible = true;
+	}
+	chart.render();
+}
+
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="canvasjs/canvasjs.min.js"></script>
+</body>
+</html>
+<form action="?task=heute" method="post" data-ajax='false'>
+    <select name="ausgewStation[]" id="select-custom-24" data-native-menu="false" multiple="multiple" data-iconpos="left">
+        <option>Ortsauswahl</option>
+        <?php
+        $ausgewStat=core::$view->ausgewStat;
+        foreach ($ausgewStat as $as){
+        echo("<option value=".$as['id']." >".$as['stationsname']."</option>\n");
+        }
+        ?>
+        <input type="submit" name="anzeigen" value="Anzeigen">
+    </select>
+</form>
