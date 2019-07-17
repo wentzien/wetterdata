ed3348e6a2dbca29cc85ee6948de72d350090dbe
Dennis Wentzien <wentzien@users.noreply.github.com>
2019-07-17 15:00:45 +0200

    unnötige Kommentare entfernt

--- views/view.heute.php
+++ views/view.heute.php
@@ -58,36 +58,6 @@ var chart = new CanvasJS.Chart("chartContainer", {
             },'); 
             }
             ?>
-//	{
-//		name: "Martha Vineyard",
-//		type: "spline",
-//		yValueFormatString: "#0.## °C",
-//		showInLegend: true,
-//		dataPoints: [
-//			{ x: new Date(2017,6,24), y: 20 },
-//			{ x: new Date(2017,6,25), y: 20 },
-//			{ x: new Date(2017,6,26), y: 25 },
-//			{ x: new Date(2017,6,27), y: 25 },
-//			{ x: new Date(2017,6,28), y: 25 },
-//			{ x: new Date(2017,6,29), y: 25 },
-//			{ x: new Date(2017,6,30), y: 25 }
-//		]
-//	},
-//	{
-//		name: "Nantucket",
-//		type: "spline",
-//		yValueFormatString: "#0.## °C",
-//		showInLegend: true,
-//		dataPoints: [
-//			{ x: new Date(2017,6,24), y: 22 },
-//			{ x: new Date(2017,6,25), y: 19 },
-//			{ x: new Date(2017,6,26), y: 23 },
-//			{ x: new Date(2017,6,27), y: 24 },
-//			{ x: new Date(2017,6,28), y: 24 },
-//			{ x: new Date(2017,6,29), y: 23 },
-//			{ x: new Date(2017,6,30), y: 23 }
-//		]
-//	}
     ]
 });
 chart.render();
