988889c96f864759d641ea6a46bdc3bcc0ae1a6b
Dennis Wentzien <wentzien@users.noreply.github.com>
2019-07-20 16:37:03 +0200

    view der Historie Anpassung

---
+++ controller/controller.historie_1.php
@@ -0,0 +1,64 @@
+<?php
+Core::$view->path["view1"]="views/view.historie.php";
+
+date_default_timezone_set("Europe/Berlin");
+$timestamp = time();
+$datum = date("Y-m-d",$timestamp);
+$pdo=Core::$pdo;
+$dieStation=$_POST['ausgewStation'];
+$taskerkenner=$_POST['taskerkenner'];
+
+$datumVon=$_POST['datumVon'];
+$datumBis=$_POST['datumBis'];
+if($datumVon==""){
+    $datumVon="1800-01-01";
+}
+if($datumBis==""){
+    $datumBis="2200-01-01";
+}
+if($taskerkenner!="historie"){
+    $datumVon="";
+    $datumBis="";
+}
+
+Core::$view->datumVon=$datumVon;
+Core::$view->datumBis=$datumBis;
+//Wurden Stationswerte übergenen?, wenn nicht soll der Nuter darauf hingwiesen werden dass er eine Station auswähelen soll
+If ($dieStation<>""){
+//Länge des Arrays
+$length=count($dieStation);
+Core::$view->length=$length;
+
+//Übergibt die StationsID's, Name und Temp Werte
+$i=0;
+foreach($dieStation as $stat){
+    
+    //Übergabe der StationsID
+    $stationsNummer="stationID$i";
+    Core::$view->$stationsNummer=$stat;
+    
+    //Übergabe der Temp Werte
+    $sqlheute="select * from Temperatur where station=$stat AND temp20>-273 AND ts>'$datumVon%' AND ts<'$datumBis%' AND ts order by ts asc";
+    $heute=$pdo->query($sqlheute);
+    $HeuteTempNummer="heuteTemp$i";
+    Core::$view->$HeuteTempNummer=$heute;
+    
+    //Übergabe  des Stationsnamen
+    $sqlStationName="select stationsname from Stationen where id=$stat";
+    $stationName=$pdo->query($sqlStationName);
+    $stationNameNummer="stationName$i";
+    Core::$view->$stationNameNummer=$stationName;
+    
+    $i++;
+}
+}
+else{
+Core::addError("Bitte wähle eine Station aus");
+}
+
+//Listet alle Stationen auf
+$SQLausgewStat="select * from Stationen";
+$ausgewStat=$pdo->query($SQLausgewStat);
+Core::$view->ausgewStat=$ausgewStat;
+?>
+    
--- views/view.historie.php
+++ views/view.historie.php
@@ -1,85 +1,15 @@
-<!DOCTYPE HTML>
-<html>
-<head>  
-<script>
-window.onload = function () {
-
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	title:{
-		text: "Historisches Wetter"
-	},
-	axisX: {
-		valueFormatString: "MM-YY"
-	},
-	axisY: {
-		title: "Temperature (in °C)",
-		includeZero: false,
-		suffix: " °C"
-	},
-	legend:{
-		cursor: "pointer",
-		fontSize: 16,
-		itemclick: toggleDataSeries
-	},
-	toolTip:{
-		shared: true
-	},
-	data: [
-            <?php 
-            $length=core::$view->length;
-            $length=$length-1;
-            for($x=0; $x<=$length; $x++){
-            echo('{
-		name: "');
-                        //Ausgabe des Stationsnamen
-                        $row1="";
-                        $stationNameNummer="stationName$x";
-                        $stationName=core::$view->$stationNameNummer;
-                        foreach($stationName as $row1){
-                        echo($row1['stationsname']);
-                        }   
-                        //Ende Ausgabe des Stationsnamen
-            echo('",
-		type: "spline",
-		yValueFormatString: "#0.## °C",
-		showInLegend: true,
-		dataPoints: ['); 
-                        //Ausgabe der Temp Werte
-                        $row2="";
-                        $heuteTempNummer="heuteTemp$x";
-                        $heuteTemp=core::$view->$heuteTempNummer;
-                        $i="1";
-                        foreach ($heuteTemp as $row2){
-                        echo("{ x: new Date (".$row2['canvasts']."), y: ".$row2['temp20']." },\n");
-                        }
-                        //Ende der Ausgabe der Temp Werte
-            echo(']
-            },'); 
-            }
-            ?>
-    ]
-});
-chart.render();
-
-function toggleDataSeries(e){
-	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-		e.dataSeries.visible = false;
-	}
-	else{
-		e.dataSeries.visible = true;
-	}
-	chart.render();
-}
-
-}
-</script>
-</head>
-<body>
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<!--CanvasJs folder-->
 <script src="canvasjs/canvasjs.min.js"></script>
-</body>
-</html>
+
+<!--Header-->
+<div data-role="header">
+        <h1>Der heutige Tag</h1>
+</div>
+<br>
+
+<!--Ortsauswahl-->
+<!--Datumsfilterung-->
+
 <form action="?task=historie" method="post" data-ajax='false'>
     <select name="ausgewStation[]" id="select-custom-24" data-native-menu="false" multiple="multiple" data-iconpos="left">
         <option>Ortsauswahl</option>
@@ -97,5 +27,313 @@ function toggleDataSeries(e){
     <input type="date" name="datumBis" id="datumBis" value="<?=core::$view->datumBis ?>"/>
     <input type="submit" name="anzeigen" value="Anzeigen">
 </form>
+<br>
 
+<!--Temperaturanzeige-->
 
+<div data-role="header" data-theme="b">
+    <h1>Temperatur</h1>
+</div>
+<div id="chartContainerTemp" style="height: 370px; width: 100%;"></div><br>
+
+<!--Luftdruckanzeige-->
+
+<div data-role="header" data-theme="b">
+    <h1>Luftdruck</h1>
+</div>
+<div id="chartContainerDruck" style="height: 370px; width: 100%;"></div><br>
+
+<!--Luftfeuchtigkeit-->
+
+<div data-role="header" data-theme="b">
+    <h1>Relative Luftfeuchtigkeit</h1>
+</div>
+<div id="chartContainerFeuchte" style="height: 370px; width: 100%;"></div><br>
+
+<!--Taupunkttemperatur-->
+
+<div data-role="header" data-theme="b">
+    <h1>Taupunkttemperatur</h1>
+</div>
+<div id="chartContainerTau" style="height: 370px; width: 100%;"></div><br>
+
+<!--Footer-->
+
+<div data-role="footer">
+    <h4>Powered by Hochschule Pforzheim</h4>
+</div>
+
+<script>
+window.onload = function () {
+
+//Temperaturchart
+var chart1 = new CanvasJS.Chart("chartContainerTemp", {
+	animationEnabled: true,
+        zoomEnabled: true,
+//	title:{
+//		text: "Das heutige Wetter"
+//	},
+	axisX: {
+		valueFormatString: "HH:mm"
+	},
+	axisY: {
+		title: "Temperature (in °C)",
+		includeZero: false,
+		suffix: " °C"
+	},
+	legend:{
+		cursor: "pointer",
+		fontSize: 16,
+		itemclick: toggleDataSeriesTemp
+	},
+	toolTip:{
+		shared: true
+	},
+	data: [
+            <?php 
+            $length=core::$view->length;
+            $length=$length-1;
+            for($x=0; $x<=$length; $x++){
+            echo('{
+		name: "');
+                        //Ausgabe des Stationsnamen
+                        $row1="";
+                        $stationNameTemp="stationTemp$x";
+                        $stationTemp=core::$view->$stationNameTemp;
+                        foreach($stationTemp as $row1){
+                        echo($row1['stationsname']);
+                        }   
+                        //Ende Ausgabe des Stationsnamen
+            echo('",
+		type: "spline",
+		yValueFormatString: "#0.## °C",
+		showInLegend: true,
+		dataPoints: ['); 
+                        //Ausgabe der Temp Werte
+                        $row2="";
+                        $heuteTempNummer="heuteTemp$x";
+                        $heuteTemp=core::$view->$heuteTempNummer;
+                        foreach ($heuteTemp as $row2){
+                        echo("{ x: new Date (".$row2['canvasts']."), y: ".$row2['temp20']." },\n");
+                        }
+                        //Ende der Ausgabe der Temp Werte
+            echo(']
+            },'); 
+            }
+            ?>
+    ]
+});
+chart1.render();
+
+function toggleDataSeriesTemp(e){
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	}
+	else{
+		e.dataSeries.visible = true;
+	}
+	chart1.render();
+}
+
+//Luftdruck
+var chart2 = new CanvasJS.Chart("chartContainerDruck", {
+	animationEnabled: true,
+        zoomEnabled: true,
+//	title:{
+//		text: "Das heutige Wetter"
+//	},
+	axisX: {
+		valueFormatString: "HH:mm"
+	},
+	axisY: {
+		title: "Luftdruck (in hPa)",
+		includeZero: false,
+		suffix: " hPa"
+	},
+	legend:{
+		cursor: "pointer",
+		fontSize: 16,
+		itemclick: toggleDataSeriesDruck
+	},
+	toolTip:{
+		shared: true
+	},
+	data: [
+            <?php 
+            for($x=0; $x<=$length; $x++){
+            echo('{
+		name: "');
+                        //Ausgabe des Stationsnamen
+                        $row1="";
+                        $stationNameDruck="stationDruck$x";
+                        $stationDruck=core::$view->$stationNameDruck;
+                        foreach($stationDruck as $row1){
+                        echo($row1['stationsname']);
+                        }   
+                        //Ende Ausgabe des Stationsnamen
+            echo('",
+		type: "spline",
+		yValueFormatString: "#0.## hPa",
+		showInLegend: true,
+		dataPoints: ['); 
+                        //Ausgabe der Temp Werte
+                        $row2="";
+                        $heuteDruckNummer="heuteDruck$x";
+                        $heuteDruck=core::$view->$heuteDruckNummer;
+                        foreach ($heuteDruck as $row2){
+                        echo("{ x: new Date (".$row2['canvasts']."), y: ".$row2['Luftdruck']." },\n");
+                        }
+                        //Ende der Ausgabe der Temp Werte
+            echo(']
+            },'); 
+            }
+            ?>
+    ]
+});
+chart2.render();
+
+function toggleDataSeriesDruck(e){
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	}
+	else{
+		e.dataSeries.visible = true;
+	}
+	chart2.render();
+}
+
+//Luftfeuchtigkeit
+var chart3 = new CanvasJS.Chart("chartContainerFeuchte", {
+	animationEnabled: true,
+        zoomEnabled: true,
+//	title:{
+//		text: "Das heutige Wetter"
+//	},
+	axisX: {
+		valueFormatString: "HH:mm"
+	},
+	axisY: {
+		title: "relative Feuchte (in %)",
+		includeZero: false,
+		suffix: " %"
+	},
+	legend:{
+		cursor: "pointer",
+		fontSize: 16,
+		itemclick: toggleDataSeriesFeuchte
+	},
+	toolTip:{
+		shared: true
+	},
+	data: [
+            <?php 
+            for($x=0; $x<=$length; $x++){
+            echo('{
+		name: "');
+                        //Ausgabe des Stationsnamen
+                        $row1="";
+                        $stationNameFeuchte="stationFeuchte$x";
+                        $stationFeuchte=core::$view->$stationNameFeuchte;
+                        foreach($stationFeuchte as $row1){
+                        echo($row1['stationsname']);
+                        }   
+                        //Ende Ausgabe des Stationsnamen
+            echo('",
+		type: "column",
+		yValueFormatString: "#0.## %",
+		showInLegend: true,
+		dataPoints: ['); 
+                        //Ausgabe der Temp Werte
+                        $row2="";
+                        $heuteFeuchteNummer="heuteFeuchte$x";
+                        $heuteFeuchte=core::$view->$heuteFeuchteNummer;
+                        foreach ($heuteFeuchte as $row2){
+                        echo("{ x: new Date (".$row2['canvasts']."), y: ".$row2['feuchte']." },\n");
+                        }
+                        //Ende der Ausgabe der Temp Werte
+            echo(']
+            },'); 
+            }
+            ?>
+    ]
+});
+chart3.render();
+
+function toggleDataSeriesFeuchte(e){
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	}
+	else{
+		e.dataSeries.visible = true;
+	}
+	chart3.render();
+}
+
+//Taupunkttemperatur
+var chart4 = new CanvasJS.Chart("chartContainerTau", {
+	animationEnabled: true,
+        zoomEnabled: true,
+//	title:{
+//		text: "Das heutige Wetter"
+//	},
+	axisX: {
+		valueFormatString: "HH:mm"
+	},
+	axisY: {
+		title: "Taupunkttemperatur (in °C)",
+		includeZero: false,
+		suffix: " °C"
+	},
+	legend:{
+		cursor: "pointer",
+		fontSize: 16,
+		itemclick: toggleDataSeriesTau
+	},
+	toolTip:{
+		shared: true
+	},
+	data: [
+            <?php 
+            for($x=0; $x<=$length; $x++){
+            echo('{
+		name: "');
+                        //Ausgabe des Stationsnamen
+                        $row1="";
+                        $stationNameTau="stationTau$x";
+                        $stationTau=core::$view->$stationNameTau;
+                        foreach($stationTau as $row1){
+                        echo($row1['stationsname']);
+                        }   
+                        //Ende Ausgabe des Stationsnamen
+            echo('",
+		type: "spline",
+		yValueFormatString: "#0.## °C",
+		showInLegend: true,
+		dataPoints: ['); 
+                        //Ausgabe der Temp Werte
+                        $row2="";
+                        $heuteTauNummer="heuteTaupunkt$x";
+                        $heuteTau=core::$view->$heuteTauNummer;
+                        foreach ($heuteTau as $row2){
+                        echo("{ x: new Date (".$row2['canvasts']."), y: ".$row2['taupunkt']." },\n");
+                        }
+                        //Ende der Ausgabe der Temp Werte
+            echo(']
+            },'); 
+            }
+            ?>
+    ]
+});
+chart4.render();
+
+function toggleDataSeriesTau(e){
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	}
+	else{
+		e.dataSeries.visible = true;
+	}
+	chart4.render();
+}
+}
+</script>
