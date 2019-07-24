512ebefc4ee596edcd7969ccdbe8840a1a964a60
Dennis Wentzien <wentzien@users.noreply.github.com>
2019-07-24 11:00:56 +0200

    Grundlage der Statistik

--- controller/controller.statistik.php
+++ controller/controller.statistik.php
@@ -1,2 +1,121 @@
 <?php
 Core::$view->path["view1"]="views/view.statistik.php";
+
+date_default_timezone_set("Europe/Berlin");
+$timestamp = time();
+$datum = date("Y-m-d",$timestamp);
+$pdo=Core::$pdo;
+$dieStation=$_POST['ausgewStation'];
+$taskerkenner=$_POST['taskerkenner'];
+
+$datumVon=$_POST['datumVon'];
+$datumBis=$_POST['datumBis'];
+if($datumVon==""){
+    $datumVon="1800-01-01";
+}
+if($datumBis==""){
+    $datumBis="2200-01-01";
+}
+if($taskerkenner!="historie"){
+    $datumVon="";
+    $datumBis="";
+}
+
+Core::$view->datumVon=$datumVon;
+Core::$view->datumBis=$datumBis;
+//Wurden Stationswerte übergenen?, wenn nicht soll der Nuter darauf hingwiesen werden dass er eine Station auswähelen soll
+If ($dieStation<>""){
+//Länge des Arrays
+$length=count($dieStation);
+Core::$view->length=$length;
+
+//Übergibt die StationsID's, Name und Temp Werte
+$i=0;
+foreach($dieStation as $stat){
+    
+    //Übergabe der StationsID
+    $stationsNummer="stationID$i";
+    Core::$view->$stationsNummer=$stat;
+    
+    //Übergabe der Temp Werte
+    $sqltemp="select temp20, canvasts from Temperatur where station=$stat AND temp20>-273 AND ts>'$datumVon%' AND ts<'$datumBis%' AND ts order by ts asc";
+    $tempheute=$pdo->query($sqltemp);
+    $HeuteTempNummer="heuteTemp$i";
+    Core::$view->$HeuteTempNummer=$tempheute;
+
+    //Übergabe des Luftdrucks
+    $sqldruck="select Luftdruck, canvasts from Temperatur where station=$stat AND Luftdruck>-999 AND ts>'$datumVon%' AND ts<'$datumBis%' AND ts order by ts asc";
+    $druckheute=$pdo->query($sqldruck);
+    $HeuteDruckNummer="heuteDruck$i";
+    Core::$view->$HeuteDruckNummer=$druckheute;
+    //Ende Übergabe des Luftdrucks
+
+    //Übergabe Luftfeuchtigkeit
+    $sqlfeuchte="select feuchte, canvasts from Temperatur where station=$stat AND feuchte>-999 AND ts>'$datumVon%' AND ts<'$datumBis%' AND ts order by ts asc";
+    $feuchteheute=$pdo->query($sqlfeuchte);
+    $HeuteFeuchteNummer="heuteFeuchte$i";
+    Core::$view->$HeuteFeuchteNummer=$feuchteheute;
+    //Ende Übergabe der Luftfeuchtigkeit
+
+    //Übergabe des Taupunktes
+    $sqltaupunkt="select taupunkt, canvasts from Temperatur where station=$stat AND taupunkt>-999 AND ts>'$datumVon%' AND ts<'$datumBis%' AND ts order by ts asc";
+    $taupunktheute=$pdo->query($sqltaupunkt);
+    $HeuteTaupunktNummer="heuteTaupunkt$i";
+    Core::$view->$HeuteTaupunktNummer=$taupunktheute;
+    //Ende Übergabe des Taupunkt
+    
+    //Übergabe Niederschlag
+    $sqlniederschlag="select RS, canvasts from niederschlagdaily where station=$stat AND RS>-999 AND ts>'$datumVon%' AND ts<'$datumBis%' AND ts order by ts asc";
+    $niederschlagheute=$pdo->query($sqlniederschlag);
+    $HeuteniederschlagNummer="heuteniederschlag$i";
+    Core::$view->$HeuteniederschlagNummer=$niederschlagheute;
+    //Ende Übergabe der Niederschlag
+
+    //Übergabe  des Stationsnamen an TempDiagramm
+    $sqlStationTemp="select stationsname from Stationen where id=$stat";
+    $stationTemp=$pdo->query($sqlStationTemp);
+    $stationNameTemp="stationTemp$i";
+    Core::$view->$stationNameTemp=$stationTemp;
+    //Ende Übergabe Stationsnamen
+
+    //Übergabe des Stationsnamen an DruckDiagramm
+    $sqlStationDruck="select stationsname from Stationen where id=$stat";
+    $stationDruck=$pdo->query($sqlStationDruck);
+    $stationNameDruck="stationDruck$i";
+    Core::$view->$stationNameDruck=$stationDruck;
+    //Ende Übergabe des Stationsnamen
+
+    //Übergabe des Stationsnamen an FeuchteDiagramm
+    $sqlStationFeuchte="select stationsname from Stationen where id=$stat";
+    $stationFeuchte=$pdo->query($sqlStationFeuchte);
+    $stationNameFeuchte="stationFeuchte$i";
+    Core::$view->$stationNameFeuchte=$stationFeuchte;
+    //Ende Übergabe des Stationsnamen
+
+    //Übergabe des Stationsnamen an TauDiagramm
+    $sqlStationTau="select stationsname from Stationen where id=$stat";
+    $stationTau=$pdo->query($sqlStationTau);
+    $stationNameTau="stationTau$i";
+    Core::$view->$stationNameTau=$stationTau;
+    //Ende Übergabe des Stationsnamen
+    
+    //Übergabe des Stationsnamen an NiederschlagDiagramm
+    $sqlStationniederschlag="select stationsname from Stationen where id=$stat";
+    $stationniederschlag=$pdo->query($sqlStationniederschlag);
+    $stationNameniederschlag="stationniederschlag$i";
+    Core::$view->$stationNameniederschlag=$stationniederschlag;
+    //Ende Übergabe des Stationsnamen
+    
+    $i++;
+}
+}
+else{
+Core::addError("Bitte wähle eine Station aus");
+}
+
+//Listet alle Stationen auf
+$SQLausgewStat="select * from Stationen";
+$ausgewStat=$pdo->query($SQLausgewStat);
+Core::$view->ausgewStat=$ausgewStat;
+?>
+    
--- views/view.statistik.php
+++ views/view.statistik.php
@@ -1,1 +1,322 @@
-Hier wird eine Statistik zu den Wetter Daten angezeig.
+<div data-role="header" data-theme="b">
+    <h1>Vergleiche die Stationen bis ins Detail</h1>
+</div>
+
+<!--Ortsauswahl-->
+<!--Datumsfilterung-->
+
+<form action="?task=historie" method="post" data-ajax='false'>
+    <select name="ausgewStation[]" id="select-custom-24" data-native-menu="false" multiple="multiple" data-iconpos="left">
+        <option>Ortsauswahl</option>
+        <?php
+        $ausgewStat=core::$view->ausgewStat;
+        foreach ($ausgewStat as $as){
+        echo("<option value=".$as['id']." >".$as['stationsname']."</option>\n");
+        }
+        ?>
+    </select>
+    <input type="hidden" name="taskerkenner"  id="taskerkenner" value="historie"/>
+    <label for="datumVon">Zeitraum von:</label>
+    <input type="date" name="datumVon" id="datumVon" value="<?=core::$view->datumVon ?>"/>
+    <label for="datumVon">Zeitraum bis:</label>
+    <input type="date" name="datumBis" id="datumBis" value="<?=core::$view->datumBis ?>"/>
+    <input type="submit" name="anzeigen" value="Anzeigen">
+</form>
+
+<div data-role="header" data-theme="b">
+    <h1>Die Statistik über den ausgewählten Zeitraum</h1>
+</div>
+<center>
+    <div class="ui-body ui-body-a">
+      <table class="ui-responsive" cellpadding="10">
+          <tr>
+            <th></th>
+            <!--Stationsnamen in die Tabelle schreiben-->
+            <?php
+                $length=core::$view->length;
+                for($t=0; $t<$length; $t++){
+                $row="";
+                $stationNameTabelle="stationTabelle$t";
+                $stationTabelle=core::$view->$stationNameTabelle;
+                foreach($stationTabelle as $row){
+                $stationNT=$row['stationsname'];
+                echo("<th>$stationNT</th><th></th>");
+                }
+                }
+            ?>
+          </tr>
+          <tr>
+            <td>Aktuellster Temperaturwert</td>
+            <?php
+            for($t=0; $t<$length; $t++){
+                $row="";
+                $NameAktTemp="aktTemp$t";
+                $AktTemp=core::$view->$NameAktTemp;
+                foreach($AktTemp as $row){
+                $akttemp=$row['temp20'];
+                $akttempZeit=$row['ts'];
+                $akttempZeitrest = substr($akttempZeit, -8);
+                echo("<td><strong>$akttemp °C</strong></td><td>$akttempZeitrest</td>");
+                }
+                }
+            ?>
+           </tr>
+           <tr>
+            <td>Temperaturdurchschnitt</td>
+            <?php
+            for($t=0; $t<$length; $t++){
+                $row="";
+                $NameAvgTemp="avgTemp$t";
+                $AvgTemp=core::$view->$NameAvgTemp;
+                foreach($AvgTemp as $row){
+                $avgtemp=$row['AVG(temp20)'];
+                $avgtempRound=round( $avgtemp, 2, PHP_ROUND_HALF_UP);
+                echo("<td><strong>$avgtempRound °C</strong></td><td>-</td>");
+                }
+                }
+            ?>
+           </tr>
+           <tr>
+            <td>Max-Temperatur</td>
+            <?php
+            for($t=0; $t<$length; $t++){
+                $row="";
+                $NameMaxTemp="maxTemp$t";
+                $MaxTemp=core::$view->$NameMaxTemp;
+                foreach($MaxTemp as $row){
+                $maxtemp=$row['temp20'];
+                $maxtempZeit=$row['ts'];
+                $maxtempZeitrest = substr($maxtempZeit, -8);
+                $maxtempRound=round( $maxtemp, 2, PHP_ROUND_HALF_UP);
+                echo("<td><strong>$maxtempRound °C</strong></td><td>$maxtempZeitrest</td>");
+                }
+                }
+            ?>
+           </tr>
+           <tr>
+            <td>Min-Temperatur</td>
+            <?php
+            for($t=0; $t<$length; $t++){
+                $row="";
+                $NameMinTemp="minTemp$t";
+                $MinTemp=core::$view->$NameMinTemp;
+                foreach($MinTemp as $row){
+                $mintemp=$row['temp20'];
+                $mintempZeit=$row['ts'];
+                $mintempZeitrest = substr($mintempZeit, -8);
+                $mintempRound=round( $mintemp, 2, PHP_ROUND_HALF_UP);
+                echo("<td><strong>$mintempRound °C</strong></td><td>$mintempZeitrest</td>");
+                }
+                }
+            ?>
+           </tr>
+           <tr>
+            <td>Durchschnitts-Luftdruck</td>
+            <?php
+            for($t=0; $t<$length; $t++){
+                $row="";
+                $NameAvgDruck="avgDruck$t";
+                $AvgDruck=core::$view->$NameAvgDruck;
+                foreach($AvgDruck as $row){
+                $avgdruck=$row['AVG(Luftdruck)'];
+                $avgdruckRound=round( $avgdruck, 2, PHP_ROUND_HALF_UP);
+                if($avgdruckRound<=-999){
+                    $avgdruckRound="N/A";
+                }
+                echo("<td><strong>$avgdruckRound hPa</strong></td><td>-</td>");
+                }
+                }
+            ?>
+           </tr>
+           <tr>
+            <td>Max-Luftdruck</td>
+           </tr>
+           <tr>
+            <td>Min-Luftdruck</td>
+           </tr>
+           <tr>
+            <td>Durchschnitts-Luftfeuchtigkeit</td>
+            <?php
+            for($t=0; $t<$length; $t++){
+                $row="";
+                $NameAvgFeuchte="avgFeuchte$t";
+                $AvgFeuchte=core::$view->$NameAvgFeuchte;
+                foreach($AvgFeuchte as $row){
+                $avgfeuchte=$row['AVG(feuchte)'];
+                $avgfeuchteRound=round( $avgfeuchte, 2, PHP_ROUND_HALF_UP);
+                echo("<td><strong>$avgfeuchteRound %</strong></td><td>-</td>");
+                }
+                }
+            ?>
+           </tr>
+           <tr>
+            <td>Max-Luftfeuchtigkeit</td>
+           </tr>
+           <tr>
+            <td>Min-Luftdruck</td>
+           </tr>
+           <tr>
+            <td>Durchschnitts-Taupunkttemperatur</td>
+            <?php
+            for($t=0; $t<$length; $t++){
+                $row="";
+                $NameAvgTau="avgTau$t";
+                $AvgTau=core::$view->$NameAvgTau;
+                foreach($AvgTau as $row){
+                $avgtau=$row['AVG(taupunkt)'];
+                $avgtauRound=round( $avgtau, 2, PHP_ROUND_HALF_UP);
+                echo("<td><strong>$avgtauRound °C</strong></td><td>-</td>");
+                }
+                }
+            ?>
+           </tr>
+           <tr>
+            <td>Max-Taupunkttemperatur</td>
+           </tr>
+           <tr>
+            <td>Min-Luftdruck</td>
+           </tr>
+           <tr>
+            <td>Durchschnitts-Niederschlagsmenge</td>
+           </tr>
+           <tr>
+            <td>Max-Niederschlagsmenge</td>
+           </tr>
+           <tr>
+            <td>Min-Niederschlagsmenge</td>
+           </tr>
+        </table>
+    </div>
+</center>
+
+<div data-role="header" data-theme="b">
+    <h1>Statistik in Bezug auf alle erfassten Wetterdaten nach <strong>Jahrezeiten</strong></h1>
+</div>
+<center>
+    <div class="ui-body ui-body-a">
+      <table class="ui-responsive" cellpadding="10">
+          <tr>
+            <th>Temperaturstatistik</th>
+            <!--Stationsnamen in die Tabelle schreiben-->
+            <?php
+                $length=core::$view->length;
+                for($t=0; $t<$length; $t++){
+                $row="";
+                $stationNameTabelle="stationTabelle$t";
+                $stationTabelle=core::$view->$stationNameTabelle;
+                foreach($stationTabelle as $row){
+                $stationNT=$row['stationsname'];
+                echo("<th>$stationNT</th><th></th>");
+                }
+                }
+            ?>
+          </tr>
+           <tr>
+            <td>Heißester Frühling (Durchschnitt)</td>
+           </tr>
+           <tr>
+            <td>Kältester Frühling (Durchschnitt)</td>
+           </tr>
+           <tr>
+            <td>Höchste Messung im Frühling</td>
+           </tr>
+           <tr>
+            <td>Niedrigste Messung im Frühling</td>
+           </tr>
+           <tr>
+            <td>Heißester Sommer (Durchschnitt)</td>
+           </tr>
+           <tr>
+            <td>Kältester Sommer (Durchschnitt)</td>
+           </tr>
+           <tr>
+            <td>Höchste Messung im Sommer</td>
+           </tr>
+           <tr>
+            <td>Niedrigste Messung im Sommer</td>
+           </tr>
+           <tr>
+            <td>Heißester Herbst (Durchschnitt)</td>
+           </tr>
+           <tr>
+            <td>Kältester Herbst (Durchschnitt)</td>
+           </tr>
+           <tr>
+            <td>Höchste Messung im Herbst</td>
+           </tr>
+           <tr>
+            <td>Niedrigste Messung im Herbst</td>
+           </tr>
+           <tr>
+            <td>Heißester Winter (Durchschnitt)</td>
+           </tr>
+           <tr>
+            <td>Kältester Winter (Durchschnitt)</td>
+           </tr>
+           <tr>
+            <td>Höchste Messung im Winter</td>
+           </tr>
+           <tr>
+            <td>Niedrigste Messung im Winter</td>
+           </tr>
+        </table>
+    </div>
+</center>
+<center>
+    <div class="ui-body ui-body-a">
+      <table class="ui-responsive" cellpadding="10">
+          <tr>
+            <th>Niederschlagsstatistik</th>
+            <!--Stationsnamen in die Tabelle schreiben-->
+            <?php
+                $length=core::$view->length;
+                for($t=0; $t<$length; $t++){
+                $row="";
+                $stationNameTabelle="stationTabelle$t";
+                $stationTabelle=core::$view->$stationNameTabelle;
+                foreach($stationTabelle as $row){
+                $stationNT=$row['stationsname'];
+                echo("<th>$stationNT</th><th></th>");
+                }
+                }
+            ?>
+          </tr>
+           <tr>
+            <td>Menge im Frühling (Durchschnitt)</td>
+           </tr> 
+           <tr>
+            <td>Regnerischste Tag im Frühling</td>
+           </tr>
+           <tr>
+            <td>Längste Trockenzeit im Frühling</td>
+           </tr>
+           <tr>
+            <td>Menge im Sommer (Durchschnitt)</td>
+           </tr> 
+           <tr>
+            <td>Regnerischste Tag im Sommer</td>
+           </tr>
+           <tr>
+            <td>Längste Trockenzeit im Sommer</td>
+           </tr>
+           <tr>
+            <td>Menge im Herbst (Durchschnitt)</td>
+           </tr> 
+           <tr>
+            <td>Regnerischste Tag im Herbst</td>
+           </tr>
+           <tr>
+            <td>Längste Trockenzeit im Herbst</td>
+           </tr>
+           <tr>
+            <td>Menge im Winter (Durchschnitt)</td>
+           </tr> 
+           <tr>
+            <td>Regnerischste Tag im Winter</td>
+           </tr>
+           <tr>
+            <td>Längste Trockenzeit im Winter</td>
+           </tr>
+        </table>
+    </div>
+</center>
